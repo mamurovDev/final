2be0484384fe0478807f6ac77a0a9d95be750ff6
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-01-26 19:08:19 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let arr = [
+  { name: "Nurmuhammad", liked: true, role: "stud" },
+  { name: "John", liked: false, role: "professor" },
+  { name: "Doe", liked: true, role: "actor" },
+];
+
+console.log(arr.filter((item) => item.liked));
